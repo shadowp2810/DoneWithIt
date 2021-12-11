46dcbca22cf26d5b1e0b0a6f8d9b0d3911d0d1b1
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-12 03:51:56 +0530

    justifyContent alignItems alignSelf.

--- App.js
+++ App.js
@@ -1,45 +1,69 @@
 /*
-Now lets talk about Flex or Flexbox.
-With Flex we can easily build complex layouts
-that work consistently across different screen sizes.
+In this lesson we will talk about aligning items in a container.
 
-We have the same concept in css,
-but flex is css is a bit different.
-<View style={{ backgroundColor: "dodgerblue", flex: 1 }}></View>
-This sets backgroundColor to take up entire screen.
-flex: 0.5 for half screen.
+With this property justifyContent: "center",
+we can align items in the main or primary axis.
+As our flex direction is set to row, our main axis is horizontal.
+If flex direction was set to column, our items would be in center of vertical axis.
+With justifyContent we have flex-start by defaut, then flex-end, and center.
+We also have space-between, space-arround, space-evenly.
 
-With Flex we typially sets up view as a container,
-and then allign children inside that container.
-<View style={{ backgroundColor: "#fff", flex: 1 }}>
-<View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
-<View style={{ backgroundColor: "gold", flex: 1 }} />
-<View style={{ backgroundColor: "tomato", flex: 1 }} />
-<StatusBar style="auto" />
-</View>
+With property alignItems: "center",
+we can align items in the secondary or cross axis.
+With baseline the base remains same for all views regardless of height difference. 
+With flex-end items appear at end of vertical axis, and flex-start at start of vertical axis.
 
-With this screen is divided into three segments 
-and each takes up third of screen.
-
-Setting dodgerblue view to flex: 2 makes it 
-twice as big as the other views.
-<View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-With this setup we are essentially dividing space into 4 segments.
-2+1+1
+We use alignItems for whole container,
+alignSelf for individual item.
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar as StatusBarNative,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    <SafeAreaView
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop:
+          Platform.OS === "android" ? StatusBarNative.currentHeight : 0,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 200,
+          alignSelf: "flex-start",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 200,
+          alignSelf: "flex-end",
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
