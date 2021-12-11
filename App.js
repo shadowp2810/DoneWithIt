7f5265d196e8f65363d518be784d53e4fbe3a025
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-11 23:14:23 +0530

    Display local and network image.

--- App.js
+++ App.js
@@ -1,27 +1,72 @@
 /*
-Text props
-- numberOfLines​ , Here if text is long it gets truncated.
-- onPress , A function is called on press.
+We can display local(bundled into app) or network images
+
+For local image
+<Image source={require("./assets/icon.png")} />
+The require function returns a number that is a reference to our image.
+
+console.log(require("./assets/icon.png"));
+gives 4
+
+https://picsum.photos/
+is a random photo generator.
+https://picsum.photos/200/300
+generates random image of width 200 and height 300.
+
+For network images we have to specify dimensions to be visible.
+<Image
+source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+/>
+
+Image component has many props.
+https://reactnative.dev/docs/image
+
+blurRadius , the blur radius of the blur filter added to the image.
+blurRadius={10}
+
+loadingIndicatorSource​ , is similar to source prop.
+We can give local image using require function,
+or pass a network image using uri of image.
+Image we pass will be displayed as actual image is downloaded.
+
+fadeDuration , only for Android​, displays image with fade effect.
+
+resizeMode , is used if diminsions of image is different from one we specify.
+It has various modes 'cover', 'contain', 'stretch', 'repeat', 'center'.
+Same as web applications.
+
+Some useful prop methods are
+onLoad​ , Invoked when load completes successfully.
+(ImageLoadEvent) => void
+
+onLoad​End , Invoked when load either succeeds or fails.
+() => void
+
+onLoad​Start , Invoked on load start.
+() => void
+Example: onLoadStart={() => this.setState({loading: true})}
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text Pressed");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Nisi ea amet cupidatat exercitation eiusmod est nostrud velit ea qui
-        ipsum. Labore mollit minim ut duis excepteur esse. Eu elit exercitation
-        non eiusmod sint consequat Lorem. Consectetur magna voluptate aliqua
-        consectetur aliqua laboris pariatur enim ea. Sunt voluptate consectetur
-        dolor tempor in ut. Lorem id sint minim eiusmod reprehenderit id
-        occaecat laboris veniam officia nisi incididunt. Elit fugiat non
-        occaecat commodo aute nisi non.
-      </Text>
+      <Text>Hello React Native!</Text>
+      <Image
+        blurRadius={10}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -31,5 +76,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
