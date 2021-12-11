4f324a3935ad5869a6699025f26cf0ce9817f88a
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-12 00:03:00 +0530

    Display alert that takes input on button click.

--- App.js
+++ App.js
@@ -1,32 +1,42 @@
 /*
-Now we talk about the button component.
-
-Each platform has a different way of presenting this button component.
-As the button component gets mapped to its native equivalent.
-We can change color that gets effected both platforms.
+Instead of displaying something on console lets display a standard alert box.
+We replace console.log with alert.
       <Button
         color={"orange"}
         title="Click Me"
-        onPress={() => console.log("Button Tapped")}
+        onPress={() => alert("Button Tapped")}
       />
 
-We can also create buttons with unique look and feel,
-which we look at later.
+
+We import Alert, which is not a component but an api, an object with some methods.
+
+Here we get an alert message with two buttons. 
+      <Button
+        color={"orange"}
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("My Title", "My Message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+
+Now we look at prompt method which gives us an input box.
+      <Button
+        color={"orange"}
+        title="Click Me"
+        onPress={() =>
+          Alert.prompt("My Title", "My Message", (text) => console.log(text))
+        }
+      />
+Here whatever we input and hit ok calls the callback function and displays text we typed.
+Currently(2020 recording) this callback only works on ios and not and android.
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  SafeAreaView,
-  Button,
-} from "react-native";
+import { StyleSheet, Text, Alert, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   return (
@@ -35,7 +45,9 @@ export default function App() {
       <Button
         color={"orange"}
         title="Click Me"
-        onPress={() => console.log("Button Tapped")}
+        onPress={() =>
+          Alert.prompt("My Title", "My Message", (text) => console.log(text))
+        }
       />
       <StatusBar style="auto" />
     </SafeAreaView>
