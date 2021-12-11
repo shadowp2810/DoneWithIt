e173c4d793d2bd8d0e8818d635a54d2fcfa2afc8
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-12 00:56:42 +0530

    Add platform specific style property.

--- App.js
+++ App.js
@@ -1,59 +1,30 @@
 /*
-Another useful API to know about is the StyleSheet API.
-With this we define styles.
-What we have here is not css but their names are defined by css.
-They are just regular javascript properties.
+There are times we need to detect which platform is running
+and customize our styles and behaviour.
 
-So when we build the app,
-react native maps these platform agnostic components to thier native equivalent.
-And then it applies these properties on them.
+SafeAreaView only works for iOS.
+So we need to manually add some padding on top.
+This is where we add platform module.
 
-The object we pass on view or safeareaview style={styles.container} is just a ordinary js object.
-We could instead of refrencing object we can also pass an inline object here.
-style={{ backgroundColor: "orange" }}
+example
+paddingTop: Platform.OS === "android" ? 20 : 0,
 
-We can also define it somewhere else.
-const containerStyle = { backgroundColor: "orange" };
-and reference it in component
-style={containerStyle}
-
-Previously we were using styles.container.
-style={styles.container}
-where styles is the object he are defining below in under App function component.
-So when we call StyleSheet.create({ in const styles = StyleSheet.create({ ,
-we pass an object which is same object we get as a result.
-const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: "#fff",
-},
-});
-So our styles object has a property called container, which is an object itself.
-This method validates the properties we user here.
-So it ensures we don't accidentely mispell a property.
-This validation doesn't happen if we use a plain js object directly.
-Passing an inline object like style={{ backgroColor: "orange" }}
-which has errors presents us with no errors.
-So this is one benefit of using StyleSheet.create.
-
-The second benefit is the react native team
-has been working on some optimizations behind the scene.
-So it is recommended practice to use StyleSheet API to define styles.
-
-In style={styles.container} we can pass an array of objects.
-style={[styles.container, containerStyle]}
-The object on right overwrites object on left.
-So results are more predictable than css.
-
-You can extract the styles to another file,
-but its more conventional to define it below.
-Its a personal preference, but might be easier if right below defined component.
+But the proper way to do it is by caluculating and setting.
+paddingTop: Platform.OS === "android" ? StatusBarNative.currentHeight : 0,
+Where StatusBarNative was StatusBar from react-native.
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, Alert, SafeAreaView, Button } from "react-native";
-import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import {
+  StyleSheet,
+  Text,
+  Alert,
+  SafeAreaView,
+  Button,
+  Platform,
+  StatusBar as StatusBarNative,
+} from "react-native";
 
 export default function App() {
   return (
@@ -77,7 +48,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBarNative.currentHeight : 0,
   },
 });
