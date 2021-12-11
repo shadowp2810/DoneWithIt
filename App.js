846a6b287311139a2366cc81042497a6d5840cbc
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-11 22:47:40 +0530

    Add text props.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 /*
-SafeAreaView adds padding on top 
-so that content is within safe area.
+Text props
+- numberOfLines​ , Here if text is long it gets truncated.
+- onPress , A function is called on press.
 */
 
 import { StatusBar } from "expo-status-bar";
@@ -8,9 +9,19 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Nisi ea amet cupidatat exercitation eiusmod est nostrud velit ea qui
+        ipsum. Labore mollit minim ut duis excepteur esse. Eu elit exercitation
+        non eiusmod sint consequat Lorem. Consectetur magna voluptate aliqua
+        consectetur aliqua laboris pariatur enim ea. Sunt voluptate consectetur
+        dolor tempor in ut. Lorem id sint minim eiusmod reprehenderit id
+        occaecat laboris veniam officia nisi incididunt. Elit fugiat non
+        occaecat commodo aute nisi non.
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
