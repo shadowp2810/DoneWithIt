caf4c4278953aa904efc19eca2fb0465401bc0bc
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-12 03:01:36 +0530

    Give multiple flex views to app component.

--- App.js
+++ App.js
@@ -1,36 +1,45 @@
 /*
+Now lets talk about Flex or Flexbox.
+With Flex we can easily build complex layouts
+that work consistently across different screen sizes.
 
+We have the same concept in css,
+but flex is css is a bit different.
+<View style={{ backgroundColor: "dodgerblue", flex: 1 }}></View>
+This sets backgroundColor to take up entire screen.
+flex: 0.5 for half screen.
+
+With Flex we typially sets up view as a container,
+and then allign children inside that container.
+<View style={{ backgroundColor: "#fff", flex: 1 }}>
+<View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
+<View style={{ backgroundColor: "gold", flex: 1 }} />
+<View style={{ backgroundColor: "tomato", flex: 1 }} />
+<StatusBar style="auto" />
+</View>
+
+With this screen is divided into three segments 
+and each takes up third of screen.
+
+Setting dodgerblue view to flex: 2 makes it 
+twice as big as the other views.
+<View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
+With this setup we are essentially dividing space into 4 segments.
+2+1+1
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Platform,
-  StatusBar as StatusBarNative,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View, StyleSheet } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
+      <View style={{ backgroundColor: "gold", flex: 1 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
 
@@ -38,6 +47,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBarNative.currentHeight : 0,
   },
 });
