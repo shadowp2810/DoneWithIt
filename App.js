c404afed9a037eb89c3b980d5e578542c4701198
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-13 02:33:43 +0530

    Add icon to app.

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 /*
-In react native we dont have style inheritance.
-So we cannot apply text styles to parent component like in it's view.
+Can find expo vector icons here
+https://icons.expo.fyi/
 
-To make sure text looks same on all screens 
-we use a technique called encapsulating styles.
-So we encapsulate all styles inside a custom text component.
-Here lets call it AppText.
-We can do same with the Heading as with Body.
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+<MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
 */
 
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
-
-import AppText from "./app/components/AppText";
-import AppHeading from "./app/components/AppHeading";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -26,13 +22,7 @@ export default function App() {
         padding: "3%",
       }}
     >
-      <AppHeading>Hi i am lorem</AppHeading>
-      <AppText>
-        I love React Native! Aliqua officia nulla duis veniam qui dolor occaecat
-        commodo deserunt. Reprehenderit ipsum aliqua nulla ipsum. Cupidatat
-        excepteur culpa fugiat velit ex deserunt mollit sint laboris incididunt
-        minim enim ex.
-      </AppText>
+      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
     </View>
   );
 }
