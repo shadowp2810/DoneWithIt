cd4efb55fefa07da8b59524766e0fa1995f62606
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-13 23:47:38 +0530

    Build AppTextInput component.

--- App.js
+++ App.js
@@ -1,38 +1,17 @@
 /*
-We use TexInput to get input from user.
-It has some useful props like 
-autocapitalize, autocorrect, autofocus.
 
-TextInput has many props
-
-keyboardType has
-default, number-pad, decimal-pad, numeric, email-address, phone-pad
-
-secureTextEntry
-If true, the text input obscures the text entered 
-so that sensitive text like passwords stay secure. The default value is false.
-
-maxLength={20}
 */
 
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React from "react";
 
 import Screen from "./app/components/Screen";
-import { Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
-
   return (
     <Screen>
-      <Text>{firstName}</Text>
-      <TextInput
-        maxLength={20}
-        onChangeText={(text) => setFirstName(text)}
-        placeholder="First Name"
-        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
-      />
+      <AppTextInput icon="email" placeholder="Username" />
     </Screen>
   );
 }
