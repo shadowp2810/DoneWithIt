2fc3084d5fe354cafb7d740bbc1123bda11969d5
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-13 08:30:01 +0530

    Add icons to ViewImageScreen.

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
 
 // <View
