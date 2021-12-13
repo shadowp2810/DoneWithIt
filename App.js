8fa479e3f1fc8314714aab1cc59770c59f15adf6
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-13 19:53:27 +0530

    Build Icon component.

--- App.js
+++ App.js
@@ -6,10 +6,15 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColour="white" />
+    </Screen>
+  );
 }
 
 // <View
