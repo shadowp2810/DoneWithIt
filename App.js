4bba926dff3d548d9e4013ba6d17db9a4c680c6d
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-14 06:29:19 +0530

    Build the LoginScreen.

--- App.js
+++ App.js
@@ -5,31 +5,10 @@ We need modal screen to display items.
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
-
-const categories = [
-  { label: "Furniture", value: 2 },
-  { label: "Clothing", value: 5 },
-  { label: "Cameras", value: 4 },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="E-mail" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
 
 // <View
