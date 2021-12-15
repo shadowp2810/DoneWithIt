f7678f9d49b18e6e5731deda0acace1b05a30fa9
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-16 04:40:55 +0530

    Build AppFormField refactor LoginScreen.

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 /*
-We display error message only if fild is touched.
-touched is an object similar to errors object. 
+For every input field we have an onBlur, onChangeEvent and ErrorMessage.
+This is bad, this is duplication.
+We need to extract it into a reusable component.
+Formik can use react context to pass the objects like 
+handleChange, handleSubmit, errors, setFieldTouched, touched.
+import { useFormikContext } from "formik";
+And the we can consume those objects inside our custom component.
+We use the spread operator to get the other props and put them in a single object.
+Then we use the spread operator to pass those props into input component.
 */
 
 import { StatusBar } from "expo-status-bar";
