9c14eae09bd6551e7a274e7443734872b85604f5
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-16 06:55:05 +0530

    Build ListingEditScreen and AppFormPicker.

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
 /*
-Our components folder has been growing,
-and we have a mixture of reusable components.
-Some for Forms, some for Lists.
-We reorganize into different namespaces.
-We move components one by one to make sure it works.
-We move AppForm, AppFormField, ErrorMessage, 
-and SubmitButton into form folder.
-What we move is up subjective.
-We create an index.js file, 
-import all the components and re-export them.
-With this we can easily import the form component in other components.
-We can now remove three import statements from LoginForm. 
-
-We do same with lists.
+Build ListingEditScreen and AppFormPicker
 */
 
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 
-import MessagesScreen from "./app/screens/MessagesScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />;
 }
 
 // <View
