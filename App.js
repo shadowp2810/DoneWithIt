2f4e42a92abc19566f68cd8e328273254c3bb506
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-17 00:54:10 +0530

    Create a user code snippet.

--- App.js
+++ App.js
@@ -1,5 +1,31 @@
 /*
-
+To create a code snippet,
+go to Preferene->User Snippet->{The language we want}
+in our case its javascript,
+then 
+{
+  "Function Component": {
+    "prefix": "rnfc",
+    "body": [
+      "import React from 'react';",
+      "import { View, StyleSheet } from 'react-native';",
+      "",
+      "function ${TM_FILENAME_BASE}(props) {",
+      "  return (",
+      "    <View style={styles.container}>$0</View>",
+      "  );",
+      "}",
+      "",
+      "const styles = StyleSheet.create({",
+      "  container: {}",
+      "})",
+      "",
+      "export default ${TM_FILENAME_BASE};"
+    ]
+  }
+where ${TM_FILENAME_BASE} replaces with filename automatically,
+and $0 is where we want cursor to be,
+and "", for empty line space.
 */
 
 import { StatusBar } from "expo-status-bar";
