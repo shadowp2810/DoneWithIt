48f402c54e4a46fbeafe65dbf0f7239bf378c2f4
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-19 01:49:55 +0530

    Use stack navigator.

--- App.js
+++ App.js
@@ -1,34 +1,66 @@
 /*
-To implement navigation in our app,
-we are going to use a popular library called React Navigation.
-https://reactnavigation.org/
+https://reactnavigation.org/docs/hello-react-navigation
+First we install stack navigator library.
+`npm install @react-navigation/native-stack@5.3.5`
+Each navigator is implemented in a seperate library.
 
-`npm install @react-navigation/native@5`
+const Stack = createNativeStackNavigator();
+where Stack has Group, Navigator and Screen
 
-The libraries we will install now are react-native-screens and react-native-safe-area-context.
+This component is defined in the stack library
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-`expo install react-native-screens react-native-safe-area-context`
+This component is defined in the main library
+import { NavigationContainer } from '@react-navigation/native';
 
-In react navigation we have three types of navigation.
+Other navigators like tab and drawer have same structure.
 
-- Stack Navigators,
-  for taking the user from one screen to another,
-  similar to navigator in web browsers,
-  we can push or pop screens from stack.
-- Tab Navigator, for implementing tabs.
-- Drawer Navigator, for implementing drawer from left side of screen.
+Instead of rendering specific screens like tweets or tweet details,
+we are going to render StackNavigator.
+This component knows how to navigate between different screens.
 
-All these navigators are very similar.
-In this course we will use stack and tab navigators.
+Because we use a StacNavigator we get a header on top,
+and we can fully customize this. 
+Background, foreground, style of text, add buttons to right or left.
+
+Currently we need to reload app when we swap the order of StackScreens,
+because the navigator is maintained in navigation state.
 */
 
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import AppText from "./app/components/Text";
+import Screen from "./app/components/Screen";
+
+const Tweets = () => (
+  <Screen>
+    <AppText>Tweets</AppText>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <AppText>TweetDetails</AppText>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName="Tweets">
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen name="Tweets" component={Tweets} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
 
 // <View
