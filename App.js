4e430ff0e0069ba1c858fa0b67ddf8a955897aca
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-19 00:35:50 +0530

    Note on navigation in react native.

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 /*
-Currently if we want to get users location,
-we have to write a lot of same code there,
-instead we can use a custom hook.
-Using custom hooks we can encapsulate state,
-some logic arround that state,
-inside a single reusable function.
-In app folder we add a new folder called hooks.
-All hooks should start with hooks as that is the convention.
+To implement navigation in our app,
+we are going to use a popular library called React Navigation.
+https://reactnavigation.org/
 
-We can reuse this hook in many places.
+`npm install @react-navigation/native@5`
 
-If the prise we get here
-      const { granted } = await Location.requestForegroundPermissionsAsync();
-is unresolved we will have a problem, so we wrap it in a try catch.
+The libraries we will install now are react-native-screens and react-native-safe-area-context.
 
+`expo install react-native-screens react-native-safe-area-context`
+
+In react navigation we have three types of navigation.
+
+- Stack Navigators,
+  for taking the user from one screen to another,
+  similar to navigator in web browsers,
+  we can push or pop screens from stack.
+- Tab Navigator, for implementing tabs.
+- Drawer Navigator, for implementing drawer from left side of screen.
+
+All these navigators are very similar.
+In this course we will use stack and tab navigators.
 */
 
 import { StatusBar } from "expo-status-bar";
