4f09a50a35f3f6db10f56812fab27f471aec1366
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-20 23:52:01 +0530

    Note on inspecting api calls react-native-debugger

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
 /*
-Now that we have an API layer, lets get the listings from the server.
-In ListingsScreen we
-import listingsApi from "../api/listings";
-and get rid of the cardcoded listings array.
-In the component we decreate a statevariable to store the listings from the server.
-We call our api first time component gets rendered, so we useEffect hook.
-Executed only once first time component is rendered.   useEffect(()=>{}, [])
-    listingsApi.getListings() returns a primse, but we cannot passa asynch function to a useEffect hook,
-so we implement in another function.
+To inspect API calls:
+When calling Apis sometimes things don't work as we expect.
+To troubleshoot issues we want to know if our api was called or not.
+If yes we want to look at the request and response objects.
+We use another debuggin tool called react native debugger.
+The standalone app based on official debugger of React Native, and includes React Inspector / Redux DevTools
+So combines three debug tools inside a stand alone app.
+`brew install --cask react-native-debugger`
+This puts React Native Debugger.app in your /applications/ folder.
 
-We call our api, listingsApi.getListings();
-we await it, await listingsApi.getListings();
-and we get the response, const response = await listingsApi.getListings();
-the response object has a property data which contains data from server.
+When we open react-native-debugger we see 3 debuggin tools in one place,
+chrome dev tools, redux dev tools, and react dev tools.
+We can toggle any tools we don't need by right click.
+We see a message waiting for react to connect,
+and we see it because our app isn't connected to this process.
+On the top it says it is trying to connect on port 8081,
+We have to open a new window and change port number.
+We change it to 19000.
+Then we enable remote debuggin in our app.
+So js code gets executed inside this process.
 
-We need to change prop in Card component in ListingsScreen
-            image={item.image}
-to       imageUrl={item.images[0].url}
-
-We change same in Card component 
-        <Image style={styles.image} source={image} />
-to         <Image style={styles.image} source={{uri: imageUrl}} />
-We cannot pass a string, we pass a object with uri property.
+We rightclick and say enable network inspect.
+With this we can find out api requests under network tab.
+We cna filter by xhr
+We see the call to our listings api and the response from server.
+As we know remote debugging makes app slow,
+so when you are done, go back to app and stop remote debug.
 
 */
 
