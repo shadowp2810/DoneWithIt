2610c4d81ca92cb7f4cdaf039388bc03d3c1bcf1
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2021-12-20 23:01:06 +0530

    Call API using apisauce.

--- App.js
+++ App.js
@@ -1,15 +1,54 @@
 /*
-We should get rid of hardcoded routings.
-If we have a typo our app isn't going to work,
-and the only way to find it is to test all features of app.
-And if tomorrow e decide to rename this route to something else,
-we have to rename all instances of this route.
-So we store all routes in a single place in app.
+We will now connect the backend.
+in config, development.json, 
+{
+  "assetsBaseUrl": "http://192.168.1.5:9000/assets/",
+  "port": 9000
+}
+assetsBaseUrl determins where our assets or images are served from,
+We replace 192.168.0.14 with ip address of our machine, not localhost,
+We set the port to what we want,
+9000 is usually a unique port, if we have to change it we must change it in Url as well.
 
-WE use uppercase letters to indicate its a constant,
-To prevent other modules from accidently modifying this property,
-we use Object.Freeze method,
-which ensured the object we pass here cannot be modified anywher in app,
+`node index.js` to start server.
+Our backend doesn't really have a database,
+it's a fake backend.
+It was implemented this way because its a lot faster to get up and running.
+So if we restart our server we loose the data we have created.
+
+http://localhost:9000/api/listings
+
+To call apis we are going to use a library called api sauce,
+it is a wrapper arround axios,
+which is a popular library for API calls.
+It also gives us standardised errors,
+and the ability to transform request and response objects.
+https://github.com/infinitered/apisauce
+A preference for apisauce is wenever we call server,
+the underlying promise is always resolved,
+even if we have an error,
+so we don't have to add a try catch bock in our code.
+We just take the response and see if there is an error.
+`npm i apisauce@1.1.1`
+
+Now we have methods like get post put and so on.
+And whever we call server promise is always resolved.
+
+When promise is resolved we get a response object
+And instead of using a try catch block, we just check reponse.ok
+And if it is not ok we look at response.problem
+
+
+Problem is a string and it can be one of these values.
+NONE             null               200-299       No problems.
+CLIENT_ERROR     'CLIENT_ERROR'     400-499       Any non-specific 400 series error.
+SERVER_ERROR     'SERVER_ERROR'     500-599       Any 500 series error.
+TIMEOUT_ERROR    'TIMEOUT_ERROR'    ---           Server didn't respond in time.
+CONNECTION_ERROR 'CONNECTION_ERROR' ---           Server not available, bad dns.
+NETWORK_ERROR    'NETWORK_ERROR'    ---           Network not available.
+CANCEL_ERROR     'CANCEL_ERROR'     ---           Request has been cancelled. Only possible if `cancelToken` is provided in config, see axios `Cancellation`.
+
+Api sause gives us standardized errors.
 
 */
 
---
+++ app/api/client.js
@@ -0,0 +1,33 @@
+import { create } from "apisauce";
+
+//We call it and give a configuration object.
+const apiClient = create({
+  baseURL: "http://192.168.1.5:9000/api/",
+});
+
+/*
+Now we have methods like get post put and so on.
+And whever we call server promise is always resolved.
+
+When promise is resolved we get a response object
+And instead of using a try catch block, we just check reponse.ok
+And if it is not ok we look at response.problem
+
+
+Problem is a string and it can be one of these values.
+NONE             null               200-299       No problems.
+CLIENT_ERROR     'CLIENT_ERROR'     400-499       Any non-specific 400 series error.
+SERVER_ERROR     'SERVER_ERROR'     500-599       Any 500 series error.
+TIMEOUT_ERROR    'TIMEOUT_ERROR'    ---           Server didn't respond in time.
+CONNECTION_ERROR 'CONNECTION_ERROR' ---           Server not available, bad dns.
+NETWORK_ERROR    'NETWORK_ERROR'    ---           Network not available.
+CANCEL_ERROR     'CANCEL_ERROR'     ---           Request has been cancelled. Only possible if `cancelToken` is provided in config, see axios `Cancellation`.
+
+Api sause gives us standardized errors.
+*/
+
+apiClient.get("/listings").then((response) => {
+  if (!response.ok) {
+    response.problem;
+  }
+});
